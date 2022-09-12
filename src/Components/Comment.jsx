d01c09caf7e0292884962f2afb544dd0bb54530a
MasaIha <masanori.iha@gmail.com>
2022-09-12 19:35:58 -0300

    Refatoração: criando Componente Avatar

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return(
         <div className={ styles.comment }>
-            <img class="_avatar_2ovoz_21" src="https://avatars.githubusercontent.com/u/14970377?v=4" alt="" />
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/14970377?v=4"/>
 
             <div className={ styles.commentBox }>
                 <div className={ styles.commentContent }>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar(){
     return(
@@ -9,7 +10,7 @@ export function Sidebar(){
             />
         
             <div className={ styles.profile }>
-                <img className={ styles.avatar } src="https://avatars.githubusercontent.com/u/14970377?v=4" alt="" />
+                <Avatar src="https://avatars.githubusercontent.com/u/14970377?v=4"/>
                 <strong>Masanori Iha</strong>
                 <span>Web Developer</span>
             </div>
